c31ad4690022ce925040f7cd0ab22b346c8f6e0b
skookumchoocher <natebrooks004@gmail.com>
2025-10-03 22:38:07 -0500

    update auto dark mode

--- dot_config/nvim/lua/plugins/auto-dark-mode.lua
+++ dot_config/nvim/lua/plugins/auto-dark-mode.lua
@@ -1,5 +1,27 @@
---@type LazySpec
+-- @type LazySpec
 return {
   "f-person/auto-dark-mode.nvim",
-  opts = {},
+  opts = {
+    update_interval = 1000,
+    fallback = "dark",
+    set_dark_mode = function()
+      vim.api.nvim_set_option_value("background", "dark", {})
+      vim.cmd.colorscheme "catppuccin-macchiato"
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option_value("background", "light", {})
+      vim.cmd.colorscheme "catppuccin-latte"
+    end,
+  },
+  init = function()
+    -- optional: load correct theme immediately on startup
+    local mode = vim.fn.system "defaults read -g AppleInterfaceStyle 2>/dev/null"
+    if mode:match "Dark" then
+      vim.api.nvim_set_option_value("background", "dark", {})
+      vim.cmd.colorscheme "catppuccin-macchiato"
+    else
+      vim.api.nvim_set_option_value("background", "light", {})
+      vim.cmd.colorscheme "catppuccin-latte"
+    end
+  end,
 }
